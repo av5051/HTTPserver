b30721e79ddf45e672c59c3b0a7ed891e272341f
av5051 <arpitmvmsdl@gmail.com>
2026-03-21 17:39:32 +0530

    Add main.c and .gitignore

--- main.c
+++ main.c
@@ -17,7 +17,10 @@ int main(){
     }
     printf("socket created: %d\n" , server_fd);
 
-    close(server_fd);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family(AF_INET);
+    server_addr.sin_port(htons(8080));
+    server_addr.sin_addr.s_addr = INADDR_ANY;
     return 0;
 }
 
