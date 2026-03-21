03c0ed2d89e4d3833b169a8a3c1d2b7fa499d850
av5051 <arpitmvmsdl@gmail.com>
2026-03-21 17:57:07 +0530

    added bind  function

--- main.c
+++ main.c
@@ -18,9 +18,22 @@ int main(){
     printf("socket created: %d\n" , server_fd);
 
     struct sockaddr_in server_addr;
-    server_addr.sin_family(AF_INET);
-    server_addr.sin_port(htons(8080));
+    memset(&server_addr,0,sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
     server_addr.sin_addr.s_addr = INADDR_ANY;
+
+
+    if(bind(server_fd , (struct sockaddr*)&server_addr,sizeof(server_addr)) == -1){
+
+        perror("bind failed");
+        return 1;
+
+    }
+    printf("bind successful");
+
+
     return 0;
 }
 
